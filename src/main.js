82b346cf7cb6d98606971ddfe328c052df41e945
XingLoong <xing.loong@gmail.com>
2024-05-14 23:51:05 +1000

    player logic added

--- src/main.js
+++ src/main.js
@@ -1,14 +1,45 @@
+import { scaleFactor } from './constants';
 import { k } from './kaboomContext';
 
 k.loadSprite('spritesheet', './spritesheet.png', {
 	sliceX: 39,
 	sliceY: 31,
 	anims: {
-		'idle-down': 936,
-		'walk-down': { from: 936, to: 939, loop: true, speed: 8 },
-		'idle-side': 975,
-		'walk-side': { from: 975, to: 978, loop: true, speed: 8 },
-		'idle-up': 1014,
-		'walk-up': { from: 1014, to: 1017, loop: true, speed: 8 },
+		'idle-down': 940,
+		'walk-down': { from: 940, to: 943, loop: true, speed: 8 },
+		'idle-side': 979,
+		'walk-side': { from: 979, to: 982, loop: true, speed: 8 },
+		'idle-up': 1018,
+		'walk-up': { from: 1018, to: 1021, loop: true, speed: 8 },
 	},
 });
+
+k.loadSprite('map', './map.png');
+
+k.setBackground(k.Color.fromHex('311047'));
+
+k.scene('main', async () => {
+	const mapData = await (await fetch('./map.json')).json();
+	const layers = mapData.layers;
+
+	const map = k.make([k.sprite('map'), k.pos(0), k.scale(scaleFactor)]);
+
+	const player = k.make([
+		k.sprite('spritesheet', { anim: 'idle-down' }),
+		k.area({
+			shape: new k.Rect(k.vec2(0, 3), 10, 10),
+		}),
+		k.body(),
+		k.anchor('center'),
+		k.pos(),
+		k.scale(scaleFactor),
+		{
+			speed: 250,
+			direction: 'down',
+			isInDialogue: false,
+		},
+		'player',
+	]);
+});
+
+k.go('main');
